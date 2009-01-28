3c56244eda5fa6cc354cbda64de4f37218fce1cb
Neil Crookes <neil@neilcrookes.com>
2009-01-28 00:06:02 +0000

    Adding comments and refactoring code to remove dependencies on Configuration settings. Updating test case accordingly.

--- history_component/controllers/components/history.php
+++ history_component/controllers/components/history.php
@@ -1,5 +1,31 @@
 <?php
-
+/**
+ * History Component
+ *
+ * Manages a page history stack or First In, Last Out (FILO) buffer, in the
+ * user's session.
+ *
+ * Separate stacks for site and admin parts of a site.
+ *
+ * Stacks are automatically managed in beforeRender callback (beforeRender is
+ * used because Controller::pageTitle has been set by then whereas it hasn't in
+ * beforeFilter).
+ *
+ * Automatically sets a view variable with title and uri of previous page for
+ * creating "Back to ..." links in views.
+ *
+ * Provides back() method which can redirect to any previous page. In a typical
+ * app you might have an index action from where you can edit a record - after
+ * saving the record you want to go back to the index action, which is at
+ * position 1 in the stack. Whereas after deleting a record you want to go back
+ * to the index but in this case it's at position 0 because there was no render
+ * from the delete action.
+ *
+ * Back() can also accept a default if likely to be used in an action where
+ * there may not be a previous page in the session stack, for example if the
+ * user came from another site.
+ *
+ */
 class HistoryComponent extends Object {
 
   /**
@@ -7,134 +33,248 @@ class HistoryComponent extends Object {
    *
    * @var array
    */
-	var $components = array('Session');
+  var $components = array('Session', 'RequestHandler');
 
-	/**
-	 * Default settings for the component, overrideable by Settings.history array
-	 * in $config
-	 *
-	 * 'session_key' is a string for the key in the Session for storing history data
-	 * Note this will have 2 further keys, site and admin, for storing the history
-	 * stacks
-	 *
-	 * 'save_site_history' is a boolean controlling whether the uri history on
-	 * the front end should be recorded
-	 *
-	 * 'save_admin_history' is a boolean controlling whether the uri history on
-	 * the back end / admin / cms (as defined by Routing.admin) should be recorded
-	 *
-	 * @var array
-	 */
-	var $settings = array(
+  /**
+   * Default settings for the component, overrideable by settings declared when
+   * including HistoryComponent in Controller's components array, e.g.
+   * var $components = array('History' => array('session_key' => 'MyHistory'));
+   *
+   * session_key is a string for the key in the Session for storing history
+   * data Note this will have 2 further keys, site and admin, for storing the
+   * history stacks
+   *
+   * save_site_history is a boolean controlling whether the uri history on the
+   * front end should be recorded
+   *
+   * save_admin_history is a boolean controlling whether the uri history on the
+   * back end / admin / cms (as defined by Routing.admin) should be recorded
+   *
+   * default_title is a string (that is translated) that is used if the
+   * Controller::pageTitle property is not set
+   *
+   * previous_page_var is a string that is used for the view var containing the
+   * previous page info e.g.
+   *   array(
+   *     'title' => 'previous page titile',
+   *     'uri' => '/path/to/previous/page',
+   *   )
+   *
+   * @var array
+   */
+  var $_defaults = array(
     'session_key' => 'History',
-    'save_site_history' => false,
-    'save_admin_history' => false,
+    'save_site_history' => true,
+    'save_admin_history' => true,
     'default_title' => 'Previous page',
+    'previous_page_var' => 'previousPage',
   );
 
   /**
+   * Stores settings
+   *
+   * @var array
+   */
+  var $settings;
+
+  /**
+   * Stores whether current request is in the admin or site part of the app set
+   * up in initialize method
+   *
+   * @var string 'site' or 'admin'
+   */
+  var $siteOrAdmin;
+
+  /**
+   * Called automatically before controller beforeFilter. Stores reference to
+   * controller object. Merges passed config array with default settings.
+   *
+   * @param object $controller
+   * @param array $config
+   */
+  function initialize(&$controller, $config = array()) {
+
+    $this->Controller = $controller;
+
+    if (!is_array($config)) {
+      $config = array($config);
+    }
+
+    $this->settings = array_merge($this->_defaults, $config);
+
+    $this->siteOrAdmin();
+
+  }
+
+  /**
    * Called automatically after controller beforeFilter
-   * Stores refernece to controller object
-   * Merges Settings.history array in $config with default settings
    *
    * @param object $controller
    */
-	function startup(&$controller) {
-		$this->Controller = $controller;
-		$this->settings = array_merge($this->settings, Configure::read('Settings.history'));
-	}
+  function startup(&$controller) {
+  }
 
-	/**
-	 * Saves the uri and page title of the current page in a stack in the Session.
-	 * Separate stacks for front end and back end / admin / cms (as defined by
-	 * Routing.admin)
-	 *
-	 */
-	function beforeRender() {
+  /**
+   * Saves the uri and page title of the current page in a stack in the Session.
+   * Separate stacks for front end and back end / admin / cms (as defined by
+   * Routing.admin)
+   *
+   */
+  function beforeRender() {
 
-	  $uri = null;
-	  if (isset($_SERVER['REQUEST_URI'])) {
-	    $uri = $_SERVER['REQUEST_URI'];
-	  }
+    $uri = null;
 
-	  if (!is_string($uri) || empty($uri)) {
-	    return;
-	  }
+    if (isset($_SERVER['REQUEST_URI'])) {
+      $uri = $_SERVER['REQUEST_URI'];
+    }
 
-	  if (isset($this->Controller->params['bare'])) {
-	    return;
-	  }
+    if (!is_string($uri) || empty($uri)) {
+      return;
+    }
 
-	  $siteOrAdmin = Configure::read('Runtime.site_or_admin');
+    if ($this->RequestHandler->isAjax()) {
+      return;
+    }
 
+    // If configured not to save admin or site history, return
+    if ($this->siteOrAdmin == 'site' && !$this->settings['save_site_history']) {
+      return;
+    } elseif ($this->siteOrAdmin == 'admin' && !$this->settings['save_admin_history']) {
+      return;
+    }
 
-	  if ($siteOrAdmin == 'site' && !$this->settings['save_site_history']) {
-	    return;
-	  } elseif ($siteOrAdmin == 'admin' && !$this->settings['save_admin_history']) {
-	    return;
-	  }
+    // Get history from session
+    if ($this->Session->check($this->settings['session_key'].'.'.$this->siteOrAdmin)) {
+      $history = $this->Session->read($this->settings['session_key'].'.'.$this->siteOrAdmin);
+    } else {
+      $history = array();
+    }
 
-	  if ($this->Session->check($this->settings['session_key'].'.'.$siteOrAdmin)) {
-	    $history = $this->Session->read($this->settings['session_key'].'.'.$siteOrAdmin);
-	  } else {
-	    $history = array();
-	  }
+    if (isset($history[0]['uri']) && $history[0]['uri'] == $uri) {
 
-	  if (isset($history[0]['uri']) && $history[0]['uri'] == $uri) {
-	    return;
-	  } elseif (isset($history[1]['uri']) && $history[1]['uri'] == $uri) {
-	    array_shift($history);
-	  } else {
+      /**
+       * If current uri is same as last uri, i.e. page has been refreshed, do
+       * nothing
+       */
 
-  	  if (!empty($this->Controller->pageTitle)) {
-  	    $title = $this->Controller->pageTitle;
-  	  } else {
-        $title = __('Previous page', true);
-  	  }
+    } elseif (isset($history[1]['uri']) && $history[1]['uri'] == $uri) {
 
-	    array_unshift($history, array('uri' => $uri, 'title' => $title));
-	  }
+      /**
+       * If current uri is same as last but one uri, i.e. user has gone back to
+       * previous page, remove the last page from the stack
+       */
+      array_shift($history);
 
-	  $this->Session->write($this->settings['session_key'].'.'.$siteOrAdmin, $history);
+    } else {
 
-	}
+      /**
+       * Add another page into the stack
+       */
+      if (!empty($this->Controller->pageTitle)) {
+        $title = $this->Controller->pageTitle;
+      } else {
+        $title = __($this->settings['default_title'], true);
+      }
 
-	/**
-	 * Redirect to previous page
-	 *
-	 * @param integer $index Index in the stack to redirect to. Default is -1,
-	 * i.e. go back to the page before the last one. Useful for after adding or
-	 * editing something, you want to go back to index. Use 0 if in an action that
-	 * never renders anything, e.g. delete.
-	 * @param mixed $default Could be an array or string - anything that
-	 * Controller::redirect will accept. Used if index of history stack is
-	 * unavailable for any reason.
-	 */
-	function back($index = -1, $default = null) {
+      array_unshift($history, array('uri' => $uri, 'title' => $title));
 
-	  $index = abs($index);
+    }
 
-		if ($this->Session->check($this->settings['session_key'].'.'.Configure::read('Runtime.site_or_admin').'.'.$index.'.uri')) {
+    /**
+     * Update stack in session
+     */
+    $this->Session->write($this->settings['session_key'].'.'.$this->siteOrAdmin, $history);
 
-			$redirect = $this->Session->read($this->settings['session_key'].'.'.Configure::read('Runtime.site_or_admin').'.'.$index.'.uri');
+    /**
+     * Set previous page title and link view variable
+     */
+    if (isset($history[1])) {
 
-		} elseif ($default) {
+      $this->Controller->set($this->settings['previous_page_var'], $history[1]);
 
-		  $redirect = $default;
+    }
 
-		} else {
+  }
 
-		  $redirect = array('action' => 'index');
+  /**
+   * Redirect to previous page
+   *
+   * @param integer $index Index in the stack to redirect to. Default is -1,
+   * i.e. go back to the page before the last one. Useful for after adding or
+   * editing something, you want to go back to index. Use 0 if in an action that
+   * never renders anything, e.g. delete, or toggle field values etc.
+   * @param mixed $default Could be an array or string - anything that
+   * Controller::redirect will accept. Used if index of history stack is
+   * unavailable for any reason, e.g. previous page was another site.
+   * @param boolean $return Determines whether to redirect or return
+   */
+  function back($index = -1, $default = null, $return = false) {
 
-		  if (Configure::read('Runtime.site_or_admin') == 'admin') {
-		    $redirect['admin'] = true;
-		  }
+    /**
+     * index param can be negative, but stack offset isn't, so make it unsigned
+     */
+    $index = abs($index);
 
-		}
+    if ($this->Session->check($this->settings['session_key'].'.'.$this->siteOrAdmin.'.'.$index.'.uri')) {
 
-		$this->Controller->redirect($redirect);
+      /**
+       * Redirect is from the stack
+       */
+      $redirect = $this->Session->read($this->settings['session_key'].'.'.$this->siteOrAdmin.'.'.$index.'.uri');
 
-	}
+    } elseif ($default) {
+
+      /**
+       * Stack offset unavailable so use default
+       */
+      $redirect = $default;
+
+    } else {
+
+      /**
+       * Default unspecified so go back to current controller's index action
+       */
+      $redirect = array('action' => 'index');
+
+      if ($this->siteOrAdmin == 'admin') {
+        $redirect[Configure::read('Routing.admin')] = true;
+      }
+
+    }
+
+    /**
+     * Redirect or return redirect params
+     */
+    if ($return) {
+
+      return $redirect;
+
+    } else {
+
+      $this->Controller->redirect($redirect);
+
+    }
+
+  }
+
+  /**
+   * Sets HistoryComponent::siteOrAdmin property to 'site' or 'admin'
+   *
+   * @return string 'site' or 'admin'
+   */
+  function siteOrAdmin() {
+
+    if (Configure::read('Routing.admin')
+    && isset($this->Controller->params[Configure::read('Routing.admin')])
+    && $this->params[Configure::read('Routing.admin')] = Configure::read('Routing.admin')) {
+      $this->siteOrAdmin = 'admin';
+    } else {
+      $this->siteOrAdmin = 'site';
+    }
+
+    return $this->siteOrAdmin;
+
+  }
 
 }
 
--- history_component/tests/cases/components/history.test.php
+++ history_component/tests/cases/components/history.test.php
@@ -3,41 +3,45 @@ App::import('Controller', 'AppController');
 
 class HistoryTestController extends AppController {
 
-	var $testUrl = null;
+  var $testUrl = null;
 
-	function redirect($url, $status = null, $exit = true) {
-		$this->testUrl = Router::normalize($url);
-		return false;
-	}
+  function redirect($url, $status = null, $exit = true) {
+    $this->testUrl = Router::normalize($url);
+    return false;
+  }
 }
 
 class HistoryComponentTestCase extends CakeTestCase {
 
   var $defaultSettings;
-  var $thistoryKey;
+  var $historyKey;
   var $defaultTitle;
 
   function setUp() {
     $this->Controller = new HistoryTestController();
     $this->Controller->Component->init($this->Controller);
-    $this->defaultSettings = Configure::read('Settings.history');
+    $this->defaultSettings = array(
+      'session_key' => 'History',
+      'save_site_history' => true,
+      'save_admin_history' => true,
+      'default_title' => 'Previous page',
+      'previous_page_var' => 'previousPage',
+    );
     $this->historyKey = $this->defaultSettings['session_key'];
     $this->defaultTitle = $this->defaultSettings['default_title'];
   }
 
-  function testStartup() {
+  function testInitialize() {
     $expected = $this->defaultSettings;
-    $this->Controller->History->startup($this->Controller);
+    $this->Controller->History->initialize($this->Controller);
     $this->assertEqual($this->Controller->History->settings, $expected);
   }
 
   function testBeforeRender() {
 
-    $this->Controller->History->startup($this->Controller);
-
     $this->Controller->Session->del($this->historyKey);
 
-    Configure::write('Runtime.site_or_admin', 'site');
+    $this->Controller->History->initialize($this->Controller);
 
     /**
      * test save with no uri
@@ -58,7 +62,7 @@ class HistoryComponentTestCase extends CakeTestCase {
      * test save with valid uri
      */
     $uri1 = '/controller1/action1/param1/param2/named1:val1/named2:val2?query=string&query2=string2';
-    $expected[Configure::read('Runtime.site_or_admin')][] = array('uri' => $uri1, 'title' => __($this->defaultTitle, true));
+    $expected['site'][] = array('uri' => $uri1, 'title' => __($this->defaultTitle, true));
     $_SERVER['REQUEST_URI'] = $uri1;
     $this->Controller->History->beforeRender();
     $this->assertEqual($this->Controller->Session->read($this->historyKey), $expected);
@@ -77,27 +81,27 @@ class HistoryComponentTestCase extends CakeTestCase {
     $title2 = 'test2';
     $_SERVER['REQUEST_URI'] = $uri2;
     $this->Controller->pageTitle = $title2;
-    array_unshift($expected[Configure::read('Runtime.site_or_admin')], array('uri' => $uri2, 'title' => $title2));
+    array_unshift($expected['site'], array('uri' => $uri2, 'title' => $title2));
     $this->Controller->History->beforeRender();
     $this->assertEqual($this->Controller->Session->read($this->historyKey), $expected);
 
     /**
-     * test new uri does not get pushed onto front of stack if bare
+     * test new uri does not get pushed onto front of stack if isAjax
      */
     $uri3 = '/controller3/action';
     $title3 = 'test3';
     $_SERVER['REQUEST_URI'] = $uri3;
     $this->Controller->pageTitle = $title3;
-    $this->Controller->params['bare'] = 1;
+    $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
     $this->Controller->History->beforeRender();
     $this->assertEqual($this->Controller->Session->read($this->historyKey), $expected);
 
     /**
      * test new uri does get pushed onto front of stack if not bare
      */
-    unset($this->Controller->params['bare']);
+    $_SERVER['HTTP_X_REQUESTED_WITH'] = '';
     $this->Controller->History->beforeRender();
-    array_unshift($expected[Configure::read('Runtime.site_or_admin')], array('uri' => $uri3, 'title' => $title3));
+    array_unshift($expected['site'], array('uri' => $uri3, 'title' => $title3));
     $this->assertEqual($this->Controller->Session->read($this->historyKey), $expected);
 
     /**
@@ -106,26 +110,27 @@ class HistoryComponentTestCase extends CakeTestCase {
     $_SERVER['REQUEST_URI'] = $uri2;
     $this->Controller->pageTitle = $title2;
     $this->Controller->History->beforeRender();
-    array_shift($expected[Configure::read('Runtime.site_or_admin')]);
+    array_shift($expected['site']);
     $this->assertEqual($this->Controller->Session->read($this->historyKey), $expected);
 
     /**
      * test new uri in admin routing
      */
-    Configure::write('Runtime.site_or_admin', 'admin');
+    $this->Controller->History->siteOrAdmin = 'admin';
     $uri4 = '/'.Configure::read('Routing.admin').'/controller4/action';
     $_SERVER['REQUEST_URI'] = $uri4;
     $this->Controller->pageTitle = null;
-    $expected[Configure::read('Runtime.site_or_admin')][] = array('uri' => $uri4, 'title' => __($this->defaultTitle, true));
+    $expected['admin'][] = array('uri' => $uri4, 'title' => __($this->defaultTitle, true));
     $this->Controller->History->beforeRender();
     $this->assertEqual($this->Controller->Session->read($this->historyKey), $expected);
 
   }
 
   function testBack() {
-    $this->Controller->History->startup($this->Controller);
+
     $this->Controller->Session->del($this->historyKey);
-    Configure::write('Runtime.site_or_admin', 'site');
+
+    $this->Controller->History->initialize($this->Controller);
 
     $history = array(
       'site' => array(
@@ -163,14 +168,14 @@ class HistoryComponentTestCase extends CakeTestCase {
     /**
      * test back() with default index
      */
-    $expected = Router::normalize($history[Configure::read('Runtime.site_or_admin')][1]['uri']);
+    $expected = Router::normalize($history['site'][1]['uri']);
     $this->Controller->History->back();
     $this->assertEqual($this->Controller->testUrl, $expected);
 
     /**
      * test back() with specific index
      */
-    $expected = Router::normalize($history[Configure::read('Runtime.site_or_admin')][0]['uri']);
+    $expected = Router::normalize($history['site'][0]['uri']);
     $this->Controller->History->back(0);
     $this->assertEqual($this->Controller->testUrl, $expected);
 
@@ -179,23 +184,46 @@ class HistoryComponentTestCase extends CakeTestCase {
      */
     $default = '/default_controller/default_action';
     $expected = Router::normalize($default);
-    $this->Controller->History->back(count($history[Configure::read('Runtime.site_or_admin')]), $default);
+    $this->Controller->History->back(count($history['site']), $default);
     $this->assertEqual($this->Controller->testUrl, $expected);
 
     /**
      * test back() with non-existent index and no default
      */
     $expected = Router::normalize(array('action' => 'index'));
-    $this->Controller->History->back(count($history[Configure::read('Runtime.site_or_admin')]));
+    $this->Controller->History->back(count($history['site']));
     $this->assertEqual($this->Controller->testUrl, $expected);
 
     /**
      * test back() with non-existent index and no default and in admin
      */
-    Configure::write('Runtime.site_or_admin', 'admin');
-    $expected = Router::normalize(array('action' => 'index', 'admin' => true));
-    $this->Controller->History->back(count($history[Configure::read('Runtime.site_or_admin')]));
+    $this->Controller->History->siteOrAdmin = 'admin';
+    $expected = Router::normalize(array('action' => 'index', Configure::read('Routing.admin') => true));
+    $this->Controller->History->back(count($history['admin']));
     $this->assertEqual($this->Controller->testUrl, $expected);
+
+  }
+
+  function testSiteOrAdmin() {
+
+    $this->Controller->History->initialize($this->Controller);
+
+    unset($this->Controller->History->siteOrAdmin);
+
+    /**
+     * test siteOrAdmin() with no controller params specifically set
+     */
+    $expected = 'site';
+    $result = $this->Controller->History->siteOrAdmin();
+    $this->assertEqual($result, $expected);
+
+    /**
+     * test siteOrAdmin() with controller params set to admin
+     */
+    $this->Controller->params[Configure::read('Routing.admin')] = true;
+    $expected = 'admin';
+    $result = $this->Controller->History->siteOrAdmin();
+    $this->assertEqual($result, $expected);
 
   }
 
